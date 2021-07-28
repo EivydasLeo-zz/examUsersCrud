fbae79eb257b028dda0ac429a45b68f571bf6e7e
Your Name <you@example.com>
2021-07-28 09:33:18 +0300

    dotenv, mongoose, morgan, cors installed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
-
 import React, { Component } from 'react';
 
 class App extends Component {
   state = {};
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <h1>Hello world!</h1>
+      </div>
+    );
   }
 }
 
