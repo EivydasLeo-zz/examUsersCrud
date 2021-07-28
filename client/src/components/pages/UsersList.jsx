5ae92e29ff5cbc6d7476e287fb0facce71a06594
Your Name <you@example.com>
2021-07-28 10:41:27 +0300

    Receiving allUsers in front from back, some css styling.

--- client/src/components/pages/UsersList.jsx
+++ client/src/components/pages/UsersList.jsx
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
-
+import { getUsers } from '../../service/fetchData';
 class UsersList extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
+
+  async getAllUsers() {
+    const users = await getUsers();
+    this.setState({ data: users });
+  }
+
+  componentDidMount() {
+    this.getAllUsers();
+  }
+
   render() {
     return (
       <div className="UserList-container">
-        <div className="card bg-dark" id="cardUsersList">
-          <div className="card-body">
-            <h5 className="card-title">title</h5>
-            <h6 className="card-subtitle mb-2">Continent: </h6>
-            <h6 className="card-subtitle mb-2">Population: </h6>
-            <h6 className="card-subtitle mb-2">Preference: </h6>
-            <button className="btn btn-warning">Edit</button>
-            <button className="btn btn-danger">Delete</button>
+        {this.state.data.map((obj) => (
+          <div className="card bg-dark " id="cardUsersList" key={obj._id}>
+            <div className="card-body ">
+              <h6 className="card-title">Username: {obj.userName}</h6>
+              <h6 className="card-subtitle mb-2">Email: {obj.email} </h6>
+              <h6 className="card-subtitle mb-2">Age: {obj.age} </h6>
+              <h6 className="card-subtitle mb-2">Password: {obj.password} </h6>
+              <button className="btn btn-warning BtnEdit">Edit</button>
+              <button className="btn btn-danger BtnDelete">Delete</button>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     );
   }
