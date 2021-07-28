b0dbc2e0ada92392f5dbd9b686e5d1350ac09e10
Your Name <you@example.com>
2021-07-28 10:27:49 +0300

    Added form in NewUserForm.jsx

--- client/src/components/pages/NewUserForm.jsx
+++ client/src/components/pages/NewUserForm.jsx
@@ -5,7 +5,36 @@ class NewUserForm extends Component {
   render() {
     return (
       <div className="NewUserForm-container">
-        <h1>New User</h1>
+        <form className="row row-cols-lg-auto g-3 align-items-center">
+          <div className="col-12">
+            <label className="visually-hsidden">User Name:</label>
+            <div className="input-group">
+              <input type="text" className="form-control" placeholder="Username..." />
+            </div>
+            <label className="visually-hsidden">Age:</label>
+            <div className="input-group">
+              <input type="text" className="form-control" placeholder="Email..." />
+            </div>
+            <label className="visually-hsidden">Email:</label>
+            <div className="input-group">
+              <input type="text" className="form-control" placeholder="Age..." />
+            </div>
+            <label className="visually-hsidden">Password:</label>
+            <div className="input-group">
+              <input type="text" className="form-control" placeholder="Password..." />
+            </div>
+            <label className="visually-hsidden">Re-type Password:</label>
+            <div className="input-group">
+              <input type="text" className="form-control" placeholder="Password..." />
+            </div>
+          </div>
+
+          <div className="col-12">
+            <button type="submit" className="btn btn-warning formBtn">
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     );
   }
