0f38d8275e0d82e546c6060a52541dde878bf20c
Your Name <you@example.com>
2021-07-28 10:30:41 +0300

    Added card in UserList.jsx

--- client/src/components/pages/UsersList.jsx
+++ client/src/components/pages/UsersList.jsx
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 
-class Users extends Component {
+class UsersList extends Component {
   state = {};
   render() {
     return (
-      <div className="Users-container">
-        <h1>Users</h1>
+      <div className="UserList-container">
+        <div className="card bg-dark" id="cardUsersList">
+          <div className="card-body">
+            <h5 className="card-title">title</h5>
+            <h6 className="card-subtitle mb-2">Continent: </h6>
+            <h6 className="card-subtitle mb-2">Population: </h6>
+            <h6 className="card-subtitle mb-2">Preference: </h6>
+            <button className="btn btn-warning">Edit</button>
+            <button className="btn btn-danger">Delete</button>
+          </div>
+        </div>
       </div>
     );
   }
 }
-
-export default Users;
+export default UsersList;
