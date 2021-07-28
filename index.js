f137f113063eaf5dbbc3445a8d87de299ffd3dc4
Your Name <you@example.com>
2021-07-28 09:42:05 +0300

    Creating new users in back end works

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-
+const UserModel = require('./models/UserSchema');
 const app = express();
 
 const PORT = 4000;
@@ -17,9 +17,27 @@ mongoose
 
 // MIddleware
 app.use(morgan('dev'));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.status(200).json(`Serveris veikia an port ${PORT}`);
+});
+
+// create new user
+app.post('/users/new', (req, res) => {
+  console.log(req.body);
+
+  const newUser = new UserModel({
+    userName: 'Tomas30',
+    age: 30,
+    email: 'Tomas@gmail.com',
+    password: 'Tomas1',
+  });
+
+  newUser
+    .save()
+    .then((result) => res.json(result))
+    .catch((err) => console.log(err));
 });
 
 app.listen(PORT, console.log(`Back end online on port ${PORT}`));
---
+++ models/UserSchema.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  userName: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const UserModel = mongoose.model('userModel', UserSchema);
+
+module.exports = UserModel;
